c86ed4c7ec387701204d2a13f997265fdf10ffeb
Ethan Lipson <ethan.lipson@columbia.edu>
2024-08-18 13:31:23 -0700

    Add reset button to flow

--- app/demos/flow/page.tsx
+++ app/demos/flow/page.tsx
@@ -3,13 +3,29 @@
 import Demo from '@/src/demos/flow/demo';
 import DemoTemplate from '@/src/components/demoTemplate';
 
+const ResetButton = () => {
+  return (
+    <button
+      onClick={() => {
+        window.location.reload();
+      }}
+      className="bg-neutral-300 text-neutral-600 rounded absolute left-2 bottom-2 p-1"
+    >
+      Reset
+    </button>
+  );
+};
+
 export default function Flow() {
   return (
-    <DemoTemplate demo={<Demo />}>
-      <h1>Flow</h1>
-      <p>Flow simulation using the Lattice-Boltzmann method</p>
-      <h3>Controls</h3>
-      <p>Draw with your mouse or finger!</p>
-    </DemoTemplate>
+    <>
+      <DemoTemplate demo={<Demo />}>
+        <h1>Flow</h1>
+        <p>Flow simulation using the Lattice-Boltzmann method</p>
+        <h3>Controls</h3>
+        <p>Draw with your mouse or finger!</p>
+      </DemoTemplate>
+      <ResetButton />
+    </>
   );
 }
